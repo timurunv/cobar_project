bf0e3538fbe084821a5329cf54c7cb62e64938a6
Dom <dominicd7@hotmail.com>
2025-04-07 18:15:41 +0200

    Remove the simulation time limit for now

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import tqdm
 from utils.keyboard_controller import KeyBoardController
 from utils.vision import get_fly_vision, get_fly_vision_raw, render_image_with_vision
 from flygym import Fly, YawOnlyCamera, SingleFlySimulation
 from flygym.arena import FlatTerrain
 import utils.cobar_fly as cobar_fly
 import cv2
-from tqdm import trange
-import numpy as np
+
+# OPTIONS
+#
+# render the camera
+# render the fly vision
+# render the raw fly vision
+#
+#
+
 
 if __name__ == "__main__":
-    run_time = 5.0
     timestep = 1e-4
 
+    # you can pass in parameters to enable different senses here
+    # head stabilisation
+    # camera could be optional - just play in fly mode
     fly = cobar_fly.CobarFly(enable_vision=True)
 
     cam = YawOnlyCamera(
@@ -31,32 +41,36 @@ if __name__ == "__main__":
 
     # run cpg simulation
     obs, info = sim.reset()
-    target_num_steps = int(run_time / sim.timestep)
     obs_hist = []
     info_hist = []
 
     #create window
     cv2.namedWindow("Simulation", cv2.WINDOW_NORMAL)
 
-    for k in trange(target_num_steps):
-        # Get observations
-        obs, reward, terminated, truncated, info = sim.step(controller.get_actions(obs))
-        obs_hist.append(obs)
-        info_hist.append(info)
+    with tqdm.tqdm(desc="running simulation") as progress_bar:
+        while True:
+            # Get observations
+            obs, reward, terminated, truncated, info = sim.step(
+                controller.get_actions(obs)
+            )
+            obs_hist.append(obs)
+            info_hist.append(info)
 
-        rendered_img = sim.render()[0]
-        if rendered_img is not None:
-            rendered_img = render_image_with_vision(rendered_img, get_fly_vision(fly))
-            rendered_img = cv2.cvtColor(rendered_img, cv2.COLOR_BGR2RGB)
-            cv2.imshow("Simulation", rendered_img)
-            cv2.waitKey(1)
+            rendered_img = sim.render()[0]
+            if rendered_img is not None:
+                rendered_img = render_image_with_vision(
+                    rendered_img, get_fly_vision(fly)
+                )
+                rendered_img = cv2.cvtColor(rendered_img, cv2.COLOR_BGR2RGB)
+                cv2.imshow("Simulation", rendered_img)
+                cv2.waitKey(1)
 
-        if controller.quit:
-            print("Simulation terminated by user.")
-            break
+            if controller.quit:
+                print("Simulation terminated by user.")
+                break
+            progress_bar.update()
 
-
-    print(f"Simulation terminated: {obs_hist[-1]['fly'][0] - obs_hist[0]['fly'][0]}")
+    print("Simulation finished")
 
     # Save video
     cam.save_video("./outputs/hybrid_controller.mp4", 0)
