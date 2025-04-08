7f17d9130c66c394578cbcd64846e58c2a6d6e12
tkclam <thomas.lam@epfl.ch>
2025-04-08 13:00:26 +0200

    remove old arenas folder

--- cobar_miniproject/arenas_folder/looming.py
+++
@@ -1,6 +0,0 @@
-from .pillars_looming import PillarsLoomingArena
-
-
-class LoomingArena(PillarsLoomingArena):
-    def __init__(self, **kwargs):
-        super().__init__(spawn_pillars=False, **kwargs)
