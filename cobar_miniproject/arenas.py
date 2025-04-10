25e011800275f41ce80ea14b6d4fbfc972eaa15b
Dom <dominicd7@hotmail.com>
2025-04-10 14:27:35 +0200

    Incorporate `ObstacleOdorArena` into arenas so we don't need to import flyvis and pytorch to access it

--- cobar_miniproject/arenas.py
+++ cobar_miniproject/arenas.py
@@ -1,9 +1,10 @@
+from typing import Callable
 import cv2
 import numpy as np
 import os
 
 from flygym.arena import FlatTerrain
-from flygym.examples.vision.arena import ObstacleOdorArena
+from flygym.arena import BaseArena
 
 
 def _get_random_target_position(
@@ -70,6 +71,121 @@ def _circ(
     color = bool(value)
     thickness = 1 if outer else -1
     cv2.circle(img, center, radius, color, thickness)
+
+class ObstacleOdorArena(BaseArena):
+    num_sensors = 4
+
+    def __init__(
+        self,
+        terrain: BaseArena,
+        obstacle_positions: np.ndarray = np.array([(7.5, 0), (12.5, 5), (17.5, -5)]),
+        obstacle_colors: np.ndarray | tuple = (0, 0, 0, 1),
+        obstacle_radius: float = 1,
+        obstacle_height: float = 4,
+        odor_source: np.ndarray = np.array([[25, 0, 2]]),
+        peak_odor_intensity: np.ndarray = np.array([[1]]),
+        diffuse_func: Callable = lambda x: x**-2,
+        marker_colors: list[tuple[float, float, float, float]] | None = None,
+        marker_size: float = 0.1,
+    ):
+        self.terrain_arena = terrain
+        self.obstacle_positions = obstacle_positions
+        self.root_element = terrain.root_element
+        self.friction = terrain.friction
+        self.obstacle_radius = obstacle_radius
+        z_offset = terrain.get_spawn_position(np.zeros(3), np.zeros(3))[0][2]
+        obstacle_colors = np.array(obstacle_colors)
+        if obstacle_colors.shape == (4,):
+            obstacle_colors = np.array(
+                [obstacle_colors for _ in range(obstacle_positions.shape[0])]
+            )
+        else:
+            assert obstacle_colors.shape == (obstacle_positions.shape[0], 4)
+
+        self.odor_source = np.array(odor_source)
+        self.peak_odor_intensity = np.array(peak_odor_intensity)
+        self.num_odor_sources = self.odor_source.shape[0]
+        if self.odor_source.shape[0] != self.peak_odor_intensity.shape[0]:
+            raise ValueError(
+                "Number of odor source locations and peak intensities must match."
+            )
+        self.odor_dim = self.peak_odor_intensity.shape[1]
+        self.diffuse_func = diffuse_func
+
+        # Add markers at the odor sources
+        if marker_colors is None:
+            rgb = np.array([255, 127, 14]) / 255
+            marker_colors = [(*rgb, 1)] * self.num_odor_sources
+        self.marker_colors = marker_colors
+        self._odor_marker_geoms = []
+        for i, (pos, rgba) in enumerate(zip(self.odor_source, marker_colors)):
+            pos = list(pos)
+            pos[2] += z_offset
+            marker_body = self.root_element.worldbody.add(
+                "body", name=f"odor_source_marker_{i}", pos=pos, mocap=True
+            )
+            geom = marker_body.add(
+                "geom", type="capsule", size=(marker_size, marker_size), rgba=rgba
+            )
+            self._odor_marker_geoms.append(geom)
+
+        # Reshape odor source and peak intensity arrays to simplify future calculations
+        _odor_source_repeated = self.odor_source[:, np.newaxis, np.newaxis, :]
+        _odor_source_repeated = np.repeat(_odor_source_repeated, self.odor_dim, axis=1)
+        _odor_source_repeated = np.repeat(
+            _odor_source_repeated, self.num_sensors, axis=2
+        )
+        self._odor_source_repeated = _odor_source_repeated
+        _peak_intensity_repeated = self.peak_odor_intensity[:, :, np.newaxis]
+        _peak_intensity_repeated = np.repeat(
+            _peak_intensity_repeated, self.num_sensors, axis=2
+        )
+        self._peak_intensity_repeated = _peak_intensity_repeated
+
+        # Add obstacles
+        self.obstacle_bodies = []
+        obstacle_material = self.root_element.asset.add(
+            "material", name="obstacle", reflectance=0.1
+        )
+        self.obstacle_z_pos = z_offset + obstacle_height / 2
+        for i in range(obstacle_positions.shape[0]):
+            obstacle_pos = [*obstacle_positions[i, :], self.obstacle_z_pos]
+            obstacle_color = obstacle_colors[i]
+            obstacle_body = self.root_element.worldbody.add(
+                "body", name=f"obstacle_{i}", mocap=True, pos=obstacle_pos
+            )
+            self.obstacle_bodies.append(obstacle_body)
+            obstacle_body.add(
+                "geom",
+                type="cylinder",
+                size=(obstacle_radius, obstacle_height / 2),
+                rgba=obstacle_color,
+                material=obstacle_material,
+            )
+
+    def get_spawn_position(
+        self, rel_pos: np.ndarray, rel_angle: np.ndarray
+    ) -> tuple[np.ndarray, np.ndarray]:
+        return self.terrain_arena.get_spawn_position(rel_pos, rel_angle)
+
+    def get_olfaction(self, antennae_pos: np.ndarray) -> np.ndarray:
+        antennae_pos_repeated = antennae_pos[np.newaxis, np.newaxis, :, :]
+        dist_3d = antennae_pos_repeated - self._odor_source_repeated  # (n, k, w, 3)
+        dist_euc = np.linalg.norm(dist_3d, axis=3)  # (n, k, w)
+        scaling = self.diffuse_func(dist_euc)  # (n, k, w)
+        intensity = self._peak_intensity_repeated * scaling  # (n, k, w)
+        return intensity.sum(axis=0)  # (k, w)
+
+    def pre_visual_render_hook(self, physics):
+        for geom, rgba in zip(self._odor_marker_geoms, self.marker_colors):
+            physics.bind(geom).rgba = np.array([*rgba[:3], 0])
+
+    def post_visual_render_hook(self, physics):
+        for geom, rgba in zip(self._odor_marker_geoms, self.marker_colors):
+            physics.bind(geom).rgba = np.array([*rgba[:3], 1])
+
+    def _get_max_floor_height(self) -> float:
+        return self.terrain_arena._get_max_floor_height()
 
 
 class OdorTargetOnlyArena(ObstacleOdorArena):
