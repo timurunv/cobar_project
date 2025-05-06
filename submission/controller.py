3254dd2f66113cdcc0be50e00b0b1c37d7d72bed
Theo-Lacroix <theo.lacroix@epfl.ch>
2025-05-06 18:17:57 +0200

    backward implementation quite functionnal

--- submission/controller.py
+++ submission/controller.py
@@ -5,6 +5,7 @@ from .olfaction import compute_olfaction_turn_bias
 from .pillar_avoidance import compute_pillar_avoidance
 from flygym.vision.retina import Retina
 from pathlib import Path
+from scipy.ndimage import gaussian_filter1d
 #python run_simulation.py --level 0 --max-steps 2000
 #python3 run_simulation.py --level 0 --max-steps 2000
 
@@ -33,9 +34,10 @@ class Controller(BaseController):
         for i in range(self.retina.num_ommatidia_per_eye):
             mask = self.retina.ommatidia_id_map == i + 1
             self.coms[i, :] = np.argwhere(mask).mean(axis=0)
-        self.last_action = [0,0]
         
+        self.velocities = []
         self.counter = 0
+        self.going_backward = False
 
     def _process_visual_observation(self, raw_obs):
         features = np.zeros((2, 3))
@@ -56,11 +58,29 @@ class Controller(BaseController):
     def get_actions(self, obs):
 
         #Vision
+        self.counter = self.counter + 1
         visual_features = self._process_visual_observation(obs)
         proximity_weight = np.clip(max(visual_features[2], visual_features[5]), 0, 0.2) / 0.2
         vision_action, object_detected = compute_pillar_avoidance(visual_features)
-        if self.counter % 100  == 0:
-            np.save(Path.cwd() /f'outputs/velocity_{self.counter}', obs['velocity'])
+        if self.counter > 5000 :
+            if self.counter % 5  == 0:
+                self.velocities.append(obs['velocity'])
+            if self.counter % 1000 == 0 :
+                velocities_array = np.array([np.array(v) for v in self.velocities])
+                smoothed_velocity = gaussian_filter1d(velocities_array[:,0], sigma=15) #take forward velocity component
+                avrg_velocity = np.mean(smoothed_velocity)
+                
+                if avrg_velocity < 5 and avrg_velocity > -5:
+                    self.going_backward = True
+                    self.velocities = []
+                else : 
+                    self.going_backward = False
+            if self.going_backward : 
+                print("je suis le roi du monde")
+                    
+                
+        #if self.counter == 50000:
+        #    np.save("/Users/theolacroix/Desktop/MA4_EPFL_Cobar/cobar_project/outputs/caca", self.velocities)
         #If object right in front, little turn towards olfaction (#TODO: or maybe add a dynamic weight to olfaction and vision?)
         # if self.action[0] == self.action[1] and object_detected:
         #     olf_action = np.ones((2,))
@@ -71,19 +91,22 @@ class Controller(BaseController):
         #TODO: for the ball, if in front, normal avoidance, if on side and of certain size, increase overall speed   
         
         #Olfaction
-        # if not object_detected:
-        #     self.action = np.ones((2,))
-        #     self.action += compute_olfaction_turn_bias(obs) # it will subtract from either side
+        if not object_detected:
+            self.action = np.ones((2,))
+            self.action += compute_olfaction_turn_bias(obs) # it will subtract from either side
 
-        # olfaction_action = np.ones((2,)) + compute_olfaction_turn_bias(obs)
-        # self.action = (1 - proximity_weight) * olfaction_action + proximity_weight * vision_action
+        olfaction_action = np.ones((2,)) + compute_olfaction_turn_bias(obs)
+        if self.going_backward : 
+            self.action = np.array([-1.0, -1.0])
+        if not self.going_backward : 
+            self.action = (1 - proximity_weight) * olfaction_action + proximity_weight * vision_action
 
         joint_angles, adhesion = step_cpg(
             cpg_network=self.cpg_network,
             preprogrammed_steps=self.preprogrammed_steps,
             action=self.action,
         )
-
+        print(self.action)
         return {
             "joints": joint_angles,
             "adhesion": adhesion,
